3eb6bfceb8da6a20a12512ec1e900ed978300341
Du, Changbin <changbin.du@gmail.com>
2012-06-02 21:57:21 +0800

    dnw: arrange code

--- src/dnw/dnw.c
+++ src/dnw/dnw.c
@@ -8,78 +8,98 @@
 
 const char* dev = "/dev/secbulk0";
 
+struct download_buffer {
+	unsigned long load_addr;  /* load address */
+	unsigned long size; /* data size */
+	unsigned char data[0];
+};
+
+static int _download_buffer(struct download_buffer *buf)
+{
+	int fd_dev = open(dev, O_WRONLY);
+	if( -1 == fd_dev)
+		printf("Can not open %s\n", dev);
+
+	printf("Writing data...\n");
+	size_t remain_size = buf->size;
+	size_t block_size = remain_size / 100;
+	size_t writed = 0;
+	while(remain_size>0) {
+		size_t to_write = remain_size > block_size ? block_size : remain_size;
+		if( to_write != write(fd_dev, (unsigned char*)buf + writed, to_write)) {
+			printf("failed!\n");
+			close(fd_dev);
+			return -1;
+		}
+		remain_size -= to_write;
+		writed += to_write;
+		printf("\r%lu%%\t %u bytes", writed*100/(buf->size), writed);
+		fflush(stdout);
+	}
+	close(fd_dev);
+	return 0;
+}
+
+static int download_file(const char *path, unsigned long load_addr)
+{
+	struct stat		file_stat;
+	struct download_buffer	*buffer = NULL;
+	unsigned long		total_size;
+	int			fd;
+
+	fd = open(path, O_RDONLY);
+	if(-1 == fd) {
+		printf("Can not open file - %s\n", path);
+		return -1;
+	}
+
+	if( -1 == fstat(fd, &file_stat) ) {
+		printf("Get file size filed!\n");
+		goto error;
+	}	
+	
+	total_size = file_stat.st_size + sizeof(struct download_buffer) + 2;
+	buffer = (typeof(buffer))malloc(total_size);
+	if(NULL == buffer) {
+		printf("malloc failed!\n");
+		goto error;
+	}
+	if( file_stat.st_size !=  read(fd, buffer->data, file_stat.st_size)) {
+		printf("Read file failed!\n");
+		goto error;
+	}
+
+	buffer->load_addr = load_addr;
+	buffer->size = total_size;
+#if 0
+	unsigned short sum = 0;
+	int i;
+	for(i=0; i<file_stat.st_size; i++) {
+		sum += buffer->data[i];
+	}
+#endif
+	return _download_buffer(buffer);
+
+error:
+	if(fd != -1)
+		close(fd);
+	if( NULL != buffer )
+		free(buffer);
+	return -1;
+}
 
 int main(int argc, char* argv[])
 {
-	unsigned char* file_buffer = NULL;
 	if( 2 != argc )	{
 		printf("Usage: dwn <filename>\n");
 		return 1;
 	}
 
-	int fd = open(argv[1], O_RDONLY);
-	if(-1 == fd) {
-		printf("Can not open file - %s\n", argv[1]);
-		return 1;
+	if (download_file(argv[1], 0x32000000) != 0) {
+		return -1;
 	}
-
-	struct stat file_stat;
-	if( -1 == fstat(fd, &file_stat) ) {
-		printf("Get file size filed!\n");
-		return 1;
-	}	
-	
-	file_buffer = (char*)malloc(file_stat.st_size+10);
-	if(NULL == file_buffer) {
-		printf("malloc failed!\n");
-		goto error;
-	}
-	if( file_stat.st_size !=  read(fd, file_buffer+8, file_stat.st_size)) {
-		printf("Read file failed!\n");
-		goto error;
-	}
-
-	printf("file name : %s\n", argv[1]);
-	printf("file size : %li bytes\n", file_stat.st_size);
-
-	int fd_dev = open(dev, O_WRONLY);
-	if( -1 == fd_dev) {
-		printf("Can not open %s\n", dev);
-		goto error;
-	}
-	
-	*((unsigned long*)file_buffer) = 0x32000000; 	//load address
-	*((unsigned long*)file_buffer+1) = file_stat.st_size+10;	//file size
-	unsigned short sum = 0;
-	int i;
-	for(i=8; i<file_stat.st_size+8; i++) {
-		sum += file_buffer[i];
-	}
-	
-	printf("Writing data...\n");
-	size_t remain_size = file_stat.st_size+10;
-	size_t block_size = remain_size / 100;
-	size_t writed = 0;
-	while(remain_size>0) {
-		size_t to_write = remain_size > block_size ? block_size:remain_size;
-		if( to_write != write(fd_dev, file_buffer+writed, to_write)) {
-			printf("failed!\n");
-			return 1;
-		}
-		remain_size -= to_write;
-		writed += to_write;
-		printf("\r%lu%%\t %u bytes     ", writed*100/(file_stat.st_size+10), writed);
-		fflush(stdout);
-		
-	}	
 
 	printf("OK\n");
 	return 0;
-
-error:
-	if(-1!=fd_dev) close(fd_dev);
-	if(fd != -1) close(fd);
-	if( NULL!=file_buffer )
-		free(file_buffer);
-	return -1;
 }
+
